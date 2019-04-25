1d4babdfbc1756bc83093d3b1b66ee38aa186884
Areeba Ahmed <areebakwas@gmail.com>
2019-04-25 07:10:40 -0500

    Initial commit

---
+++ JavaScript/images-data.js
@@ -0,0 +1,62 @@
+images = [
+    {
+      fileName: "01.jpg",
+      altText: "Hay Bales",
+      imgCaption:"I love hay bales. Took this snap on a drive through the countryside past some straw fields."
+    },
+    {
+      fileName: "02.jpg",
+      altText: "Lake",
+      imgCaption: "The lake was so calm today. We had a great view of the snow on the mountains from here."
+    },
+    {
+      fileName: "03.jpg",
+      altText: "Canyon",
+      imgCaption: "I hiked to the top of the mountain and got this picture of the canyon and trees below."
+    },
+    {
+      fileName: "04.jpg",
+      altText: "Iceberg",
+      imgCaption: "It was amazing to see an iceberg up close, it was so cold but didn’t snow today."
+    },
+    {
+      fileName: "05.jpg",
+      altText: "Desert",
+      imgCaption: "The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons."
+    },
+    {
+      fileName: "06.jpg",
+      altText: "Fall",
+      imgCaption: "Fall is coming, I love when the leaves on the trees start to change color."
+    },
+    {
+      fileName: "07.jpg",
+      altText: "Plantation",
+      imgCaption: "I drove past this plantation yesterday, everything is so green!"
+    },
+    {
+      fileName: "08.jpg",
+      altText: "Dunes",
+      imgCaption: "My summer vacation to the Oregon Coast. I love the sandy dunes!"
+    },
+    {
+      fileName: "09.jpg",
+      altText: "Countryside Lane",
+      imgCaption: "We enjoyed a quiet stroll down this countryside lane."
+    },
+    {
+      fileName: "10.jpg",
+      altText: "Sunset",
+      imgCaption: "Sunset at the coast! The sky turned a lovely shade of orange."
+    },
+    {
+      fileName: "11.jpg",
+      altText: "Cave",
+      imgCaption: "I did a tour of a cave today and the view of the landscape below was breathtaking."
+    },
+    {
+      fileName: "12.jpg",
+      altText: "Bluebells",
+      imgCaption: "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."
+    }
+  ]
